3e5644d5c27033ebad77fda638391401dbf48bc9
n00976 <n.ordecka@gmail.com>
2019-06-06 22:12:35 +0200

    update

--- 10_wyjatki/zad9.py
+++ 10_wyjatki/zad9.py
@@ -8,10 +8,21 @@ import webbrowser
 import urllib.error as ue
 
 
-site_name = input("Podaj stronę internetową, którą chcesz otworzyć:")
-if site_name[:8] == "https://" or site_name[:7] == "http://" or site_name[:3] == "www":
-    if site_name[-3:] == ".pl" or site_name[-4:] == ".com":
-        webbrowser.open(site_name)
-else:
-    raise ue.URLError("Twój adres strony jest nieprawidłowy, spróbuj zacząć od https://, http:// lub www")
+def open_url(site_name):
+    if site_name[:8] == "https://" or site_name[:7] == "http://" or site_name[:3] == "www":
+        if site_name[-3:] == ".pl" or site_name[-4:] == ".com":
+            webbrowser.open(site_name)
+    else:
+        raise ue.URLError("Twój adres strony jest nieprawidłowy, spróbuj zacząć od https://, http:// lub www")
 
+
+def main():
+    site_name = input("Podaj stronę internetową, którą chcesz otworzyć:")
+    open_url(site_name)
+
+
+if __name__ == "__main__":
+    print('Plik uruchomiony bezpośrednio')
+    main()
+else:
+    print('Plik zaimportowano jako moduł')
