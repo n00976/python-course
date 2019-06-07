34fdbc151c4c67048bab330b8824ec22373073b7
n00976 <n.ordecka@gmail.com>
2019-06-07 14:10:37 +0200

    update

--- 10_wyjatki/zad2.py
+++ 10_wyjatki/zad2.py
@@ -2,5 +2,22 @@
 # Pozwól użytkownikowi podać dowolny index oraz wartość.
 # Spróbuj w krotce podmienić wartość na zadanym indeksie. Obsłuż błąd
 
-my_tuple = (4, 8, 'f', 5.2, [5, 6], 'l', 'pizza')
 
+
+def change_item(user_index, user_element):
+    my_tuple = (4, 8, 'f', 5.2, [5, 6], 'l', 'pizza')
+    try:
+        my_tuple[user_index] = user_element
+    except TypeError as error:
+        print("Oops, nie można podmieniać elementów w krotce!")
+
+def main():
+    user_index = int(input("podaj numer ideksu, pod którym wartość ma zostać zmieniona:"))
+    user_element = input("Podaj, na co chcesz zmienić wybrany element:")
+    change_item(user_index,user_element)
+
+if __name__ == "__main__":
+    print('Plik uruchomiony bezpośrednio')
+    main()
+else:
+    print('Plik zaimportowano jako moduł')
